86e266d92f77c835f44c4053d7a1672b52b8356f
Thanasis Polychronakis <thanpolas@gmail.com>
2020-01-15 17:11:09 +0000

    update boot

--- node-ping-agent/agent/index.js
+++ node-ping-agent/agent/index.js
@@ -2,16 +2,24 @@
  * Ping Agent, launches and streams ping commands.
  */
 
-const pingLib = require('./ping-library');
+// const pingLib = require('./ping-library');
+
+const { startPing } = require('./ping-command');
+
+const eventBus = require('./event-bus');
 
 const boot = async () => {
   try {
-    const response = await pingLib.run({
-      ping_ip: '8.8.8.8',
+    await startPing('internet', {
+      pingIp: '8.8.8.8',
     });
   } catch (ex) {
     console.error('Failed:', ex);
   }
+
+  eventBus.on('internet-on_stdout', (message) => {
+    console.log('MSG:', message);
+  })
 
   console.log('done', response);
 };
