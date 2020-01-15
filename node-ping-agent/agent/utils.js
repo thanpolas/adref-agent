e2cd8fb84d5a5048dcd5e19e3d6d772a3355766e
Thanasis Polychronakis <thanpolas@gmail.com>
2020-01-15 17:10:52 +0000

    create the utils module

---
+++ node-ping-agent/agent/utils.js
@@ -0,0 +1,19 @@
+/**
+ * @fileoverview Set of handy utilities.
+ */
+
+const utils = module.exports = {};
+
+/**
+ * Sleep for a little while, helps with syncing with hardware...
+ *
+ * @param {number} ms Time to sleep for milliseconds.
+ * @return {Promise}
+ */
+utils.sleep = (ms) => {
+  return new Promise((resolve) => {
+    setTimeout(ms, () => {
+      resolve();
+    })
+  });
+}
