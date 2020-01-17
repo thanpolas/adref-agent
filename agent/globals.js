928c642b6c03a216de295048fbccb7bcc1dae5d6
Thanasis Polychronakis <thanpolas@gmail.com>
2020-01-17 00:54:37 +0000

    read yaml user config file on boot

--- agent/globals.js
+++ agent/globals.js
@@ -1,22 +1,42 @@
 /**
  * @fileoverview Global constants.
  */
+const fs = require('fs');
+const path = require('path');
+
+const yaml = require('yaml');
 
 const globals = module.exports = {};
 
-globals.isOsx = true;
 
+const userConfigFile = path.resolve(__dirname, '../../adref-config.yml');
 
-globals.token = 'osx_polas_dev';
+let userConfig = {};
+
+if (fs.existsSync(userConfigFile)) {
+  const file = fs.readFileSync(userConfigFile, 'utf8');
+  userConfig = yaml.parse(file);
+}
+
+//
+//
+// Global Settings Start
+//
+
+globals.token = userConfig.token || 'osx_polas_dev';
 
 // The API endpoint to submit to.
-globals.apiEndpoint = 'https://adref.projects.sirodoht.com/pings_two/';
+globals.apiEndpoint = userConfig.apiEndpoint ||
+  'https://adref.projects.sirodoht.com/pings_two/';
 
 // How many pings to sample for determining spike severity
 globals.spikePingSample = 5;
 
 // Every how many pings to submit to the API
-globals.apiSubmitPingsInterval = 300;
+globals.apiSubmitPingsInterval = userConfig.apiSubmitPingsInterval || 300;
 
 // Local watcher that controls the LEDs calculation interval in milliseconds.
-globals.localWatcherInterval = 5000;
+globals.localWatcherInterval = userConfig.localWatcherInterval || 5000;
+
+globals.isOsx = true;
+
