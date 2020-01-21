4b91ebb6ef57e25bd96e1c8f2cd00d5ee99f3841
Thanasis Polychronakis <thanpolas@gmail.com>
2020-01-21 01:42:43 +0000

    lint and fix ping-command module

--- agent/ping-command.js
+++ agent/ping-command.js
@@ -2,6 +2,7 @@
  * @fileoverview Responsible for launching and monitoring ping commands.
  */
 
+// eslint-disable-next-line security/detect-child-process
 const { spawn } = require('child_process');
 
 const log = require('./logger');
@@ -14,29 +15,28 @@ const ping = module.exports = {};
 /**
  * Start a ping and stream the results via eventBus.
  *
- * @param {Object} pingOpts Options for the ping command.
- *    @param {string} pingOpts.id REQUIRED A unique identifier.
- *    @param {string} pingOpts.pingIp REQUIRED The IP to ping.
+ * @param {Object} pingTarget Options for the ping command.
+ *    @param {string} pingTarget.id REQUIRED A unique identifier.
+ *    @param {string} pingTarget.pingIp REQUIRED The IP to ping.
  * @return {void}
  */
 ping.startPing = async (pingTarget) => {
-
   // Get arguments for ping based on OS.
   let pingArgs;
-   if (isOsx) {
+  if (isOsx) {
     pingArgs = ping.preparePingArgumentsOsx(pingTarget);
-   } else {
+  } else {
     pingArgs = ping.preparePingArgumentsLinux(pingTarget);
-   }
+  }
 
-  eventBus.on(pingTarget.id + '-on_close', async () => {
+  eventBus.on(`${pingTarget.id}-on_close`, async () => {
     // ping exited, restart.
     await ping.invokePing(pingTarget.id, pingArgs)
-      .catch(ping._invokePingErrorHandler);
+      .catch(ping._invokePingErrorHandler.bind(null, pingTarget.id, pingArgs));
   });
 
   await ping.invokePing(pingTarget.id, pingArgs)
-    .catch(ping._invokePingErrorHandler);
+    .catch(ping._invokePingErrorHandler.bind(null, pingTarget.id, pingArgs));
 };
 
 
@@ -93,39 +93,37 @@ ping.preparePingArgumentsLinux = (pingTarget) => {
  * @param {Array} pingArgs Arguments for ping command.
  * @return {Promise(string)} A promise with the ping's output.
  */
-ping.invokePing = async function(id, pingArgs) {
-  return new Promise(function(resolve, reject) {
-    const rawOutput = [];
-
+ping.invokePing = async (id, pingArgs) => {
+  return new Promise((resolve, reject) => {
     log.info('invokePing() :: Invoking ping with args:', pingArgs);
 
     const child = spawn('ping', pingArgs);
 
     let resolved = false;
 
-    child.stdout.on('data', function(buffer) {
+    child.stdout.on('data', (buffer) => {
       if (!resolved) {
         resolved = true;
         resolve();
       }
 
-      const emitKey = id + '-on_stdout';
+      const emitKey = `${id}-on_stdout`;
 
       eventBus.emit(emitKey, buffer.toString());
     });
 
-    child.stderr.on('data', function(buffer) {
+    child.stderr.on('data', (buffer) => {
       log.info('invokePing() :: stderr data:', buffer.toString());
     });
-    child.on('error', function(err) {
+    child.on('error', (err) => {
       log.warn('invokePing() Error:', err);
       reject(err);
     });
-    child.on('close', function(code, signal) {
+    child.on('close', (code, signal) => {
       log.info('invokePing() :: exited with code:', code, 'signal:', signal,
         '. Attempting restart...');
 
-      eventBus.emit(id + '-on_close', 'true');
+      eventBus.emit(`${id}-on_close`, 'true');
     });
 
     // listen for shutdown event
@@ -136,15 +134,17 @@ ping.invokePing = async function(id, pingArgs) {
 /**
  * Error handler for invokePing recursive call.
  *
+ * @param {string} id Token.
+ * @param {Array} pingArgs Arguments to run ping command.
  * @param {Error} error Error object.
  * @private
  */
-ping._invokePingErrorHandler = async (error) => {
+ping._invokePingErrorHandler = async (id, pingArgs, error) => {
   log.error('_invokePingErrorHandler() :: Failed to recurse and launch invokePing.',
     'Error:', error);
 
   // sleep and retry
   await sleep(2000);
-  ping.invokePing(pingArgs, id)
-    .catch(ping._invokePingErrorHandler);
+  ping.invokePing(id, pingArgs)
+    .catch(ping._invokePingErrorHandler.bind(null, id, pingArgs));
 };
