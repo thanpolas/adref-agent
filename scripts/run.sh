57daaac857cf0b9ad34f11c3c7cdaef9a25cc15b
Thanasis Polychronakis <thanpolas@gmail.com>
2020-01-23 15:46:51 +0000

    more robust shell boot script

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,29 +8,31 @@ NEW_AGENT_FILE=/home/pi/adref/adref-agent/new-adref.txt
 AGENT_SYMLINK=/home/pi/adref/adref-agent/adref-agent
 
 sudo /usr/bin/node $AGENT_SYMLINK > /home/pi/adref/adref.log 2>&1 &
+PID=$!
 
 while true
 do
+  echo "In the loop, PID: $PID"
   if test -f "$NEW_AGENT_FILE"; then
     # New Update found, kill existing agent and replace symlink.
-    sudo killall -9 adref-agent
+    sudo pkill -9 -P $PID
 
     # Delete symlink
-    rm AGENT_SYMLINK
+    rm $AGENT_SYMLINK
 
     ln -s `cat $NEW_AGENT_FILE` $AGENT_SYMLINK
 
     rm $NEW_AGENT_FILE
 
-    AGENT_PID=start_agent
-
+    sudo /usr/bin/node $AGENT_SYMLINK > /home/pi/adref/adref.log 2>&1 &
+    PID=$!
   fi
   sleep 10
 done
 
 function start_agent() {
   sudo /usr/bin/node $AGENT_SYMLINK > /home/pi/adref/adref.log 2>&1
-  PID = $!
+  PID=$!
 
   echo "FOUND PID: $PID"
   echo $PID
